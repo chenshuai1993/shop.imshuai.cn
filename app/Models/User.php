fe06b23e921c2c4a53f51fa39d632abac4575d90
shuaishuai.chen <sschend@isoftstone.com>
2019-05-08 23:21:58 +0800

    feat(controller): 验证邮箱中间键

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,8 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'email_verified',
     ];
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,5 +26,9 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified' => 'boolean',
     ];
 }
