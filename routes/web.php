4eebc342e89e0e07f5d4e8d8008f90b1dfbe5aaa
shuaishuai.chen <sschend@isoftstone.com>
2019-05-08 23:39:26 +0800

    feat(controller): 触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+        Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+        Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+        Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     });
 });
