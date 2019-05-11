4c705c65607ac532ad8a1cd56439484287771d9e
shuaishuai.chen <sschend@isoftstone.com>
2019-05-11 18:36:35 +0800

    feat(controller): 关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
 
 
 class OrdersController extends Controller
@@ -62,6 +63,9 @@ class OrdersController extends Controller
             // 将下单的商品从购物车中移除
             $skuIds = collect($items)->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+
+            //延时队列
+            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
             return $order;
         });
