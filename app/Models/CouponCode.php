ea7967beea37ccf5cb17464b115d0620249363dd
shuaishuai.chen <sschend@isoftstone.com>
2019-05-15 19:20:42 +0800

    feat(api&view): 优化优惠券模块

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -64,7 +64,7 @@ class CouponCode extends Model
         return $str.'减'.str_replace('.00', '', $this->value);
     }
 
-    public function checkAvailable($orderAmount = null)
+    public function checkAvailable(User $user, $orderAmount = null)
     {
         if (!$this->enabled) {
             throw new CouponCodeUnavailableException('优惠券不存在');
@@ -85,6 +85,28 @@ class CouponCode extends Model
         if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
             throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
         }
+
+        $used = Order::where('user_id', $user->id)
+            ->where('coupon_code_id', $this->id)
+            ->where(function($query) {
+                $query->where(function($query) {
+                    $query->whereNull('paid_at')
+                        ->where('closed', false);
+                })->orWhere(function($query) {
+                    $query->whereNotNull('paid_at')
+                        ->where('refund_status', '!=', Order::REFUND_STATUS_SUCCESS);
+                });
+            })
+            ->exists();
+        if ($used) {
+            throw new CouponCodeUnavailableException('你已经使用过这张优惠券了');
+        }
+
+//        select * from orders where user_id = xx and coupon_code_id = xx
+//            and (
+//            ( paid_at is null and closed = 0 )
+//            or ( paid_at is not null and refund_status != 'success' )
+//          )
     }
 
     //获取金额
@@ -111,6 +133,9 @@ class CouponCode extends Model
             return $this->decrement('used');
         }
     }
+
+
+    //
 
 
 
