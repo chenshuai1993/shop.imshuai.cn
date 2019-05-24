e0c0ac21a4815d8464e000470e5da77f246180b6
shuaishuai.chen <sschend@isoftstone.com>
2019-05-24 16:01:32 +0800

    feat(controller): 众筹结束逻辑

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Exceptions\InternalException;
+use App\Services\OrderService;
 
 class OrdersController extends Controller
 {
@@ -221,7 +222,7 @@ class OrdersController extends Controller
     }
 
 
-    public function handleRefund(Order $order, HandleRefundRequest $request)
+    public function handleRefund(Order $order, HandleRefundRequest $request, OrderService $orderService)
     {
 
         // 判断订单状态是否正确
@@ -237,7 +238,7 @@ class OrdersController extends Controller
                 'extra' => $extra,
             ]);
             // 调用退款逻辑
-            $this->_refundOrder($order);
+            $orderService->refundOrder($order);
         } else {
             // 将拒绝退款理由放到订单的 extra 字段中
             $extra = $order->extra ?: [];
