58609f723f25920fc4953b774e0776c7ee6f09bf
shuaishuai.chen <sschend@isoftstone.com>
2019-05-24 11:49:34 +0800

    feat(controller&view): 众筹商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -18,7 +18,7 @@ use App\Http\Requests\ApplyRefundRequest;
 use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Exceptions\CouponCodeUnavailableException;
 use App\Models\CouponCode;
-
+use App\Http\Requests\CrowdFundingOrderRequest;
 
 
 class OrdersController extends Controller
@@ -146,6 +146,18 @@ class OrdersController extends Controller
         ]);
 
         return $order;
+    }
+
+
+    // 创建一个新的方法用于接受众筹商品下单请求
+    public function crowdfunding(CrowdFundingOrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $sku     = ProductSku::find($request->input('sku_id'));
+        $address = UserAddress::find($request->input('address_id'));
+        $amount  = $request->input('amount');
+
+        return $orderService->crowdfunding($user, $address, $sku, $amount);
     }
 
 }
